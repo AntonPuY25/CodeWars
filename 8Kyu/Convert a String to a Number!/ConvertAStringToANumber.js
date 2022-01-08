11c627fbeba21d26ff95c5fdd854649f2145dd6f
Anton Kliaschonak <PuY25@bk.ru>
2022-01-08 14:52:27 +0300

    Convert a String to a Number

---
+++ 8Kyu/Convert a String to a Number!/ConvertAStringToANumber.js
@@ -0,0 +1,7 @@
+var stringToNumber = function(str){
+    return Number(str);
+}
+console.log(stringToNumber("1234"))//,1234)
+console.log(stringToNumber("605"))//, 605)
+console.log(stringToNumber("1405"))//,1405)
+console.log(stringToNumber("-7"))//,  -7)
