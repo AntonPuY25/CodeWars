5cc98d057ae34222736cec695b72afd59ec5afd4
Anton Kliaschonak <PuY25@bk.ru>
2022-01-08 17:23:14 +0300

    Largest Square Inside A Circle

---
+++ 7Kyu/Largest Square Inside A Circle/LargestSquareInsideACircle.js
@@ -0,0 +1,7 @@
+function areaLargestSquare(r) {
+    return Math.pow(r,2) + Math.pow(r,2)
+}
+
+console.log(areaLargestSquare(5))//, 50);
+console.log(areaLargestSquare(7))//, 98);
+console.log(areaLargestSquare(15))//, 450);
