c4f339fd4ce2c2bfe121af0355409e259a4ac3bb
Anton Kliaschonak <PuY25@bk.ru>
2022-01-08 17:43:30 +0300

    Number of Decimal Digits

---
+++ 7Kyu/Number of Decimal Digits/NumberOfDecimalDigits.js
@@ -0,0 +1,7 @@
+function digits(n) {
+   return n.toString().length
+}
+
+console.log(digits(5))//, 1, "Fail!");
+console.log(digits(12345))//, 5, "Fail!");
+console.log(digits(9876543210))//, 10, "Fail!");
