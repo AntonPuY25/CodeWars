fe01c69c17c5ea4395c82d8c1b7b8b067d63d97d
Anton Kliaschonak <PuY25@bk.ru>
2022-01-08 23:58:00 +0300

    Switch it Up!

---
+++ 8Kyu/Switch it Up/SwitchItUp.js
@@ -0,0 +1,8 @@
+function switchItUp(number){
+    const arr = ['Zero','One','Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten','Eleven','Twelve','Thirteen','Fourteen','Fifteen','Sixteen','Seventeen','Eighteen','Nineteen','Twenty']
+    return arr[number]
+}
+
+console.log(switchItUp(1))//,"One");
+console.log(switchItUp(3))//,"Three");
+console.log(switchItUp(5))//,"Five");
